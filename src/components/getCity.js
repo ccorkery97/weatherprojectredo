c74dc79151abb8f80396e2eb95aec765517dbdf1
ccorkery97 <ccorkery97@gmail.com>
2017-11-06 05:06:20 -0600

    weather3

--- src/components/getCity.js
+++ src/components/getCity.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import getLocation from './getLocation';
-var express = require('express');
+const express = require('express');
+// const axios = require('axios');
 
 
 var app = express();
@@ -8,7 +9,7 @@ var url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c58
 var api = "/api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c585309442241f836aee04f6f3118d";
 function getCity() {
     var urlString = url + getLocation();
-    var temp = app.get(api, function(req,res,next) {
+    var temp = app.get(urlString, function(req,res,next) {
         req.name;
         res.json();
     });
--- src/components/getTemp.js
+++ src/components/getTemp.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import getLocation from './getLocation';
-var express = require('express');
+const express = require('express');
+const { json } = require('body-parser');
+
+
+// const axios = require('axios');
 
 var app = express();
+app.use(json());
+
+
 var url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c585309442241f836aee04f6f3118d";
 var api = "/api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c585309442241f836aee04f6f3118d";
 function getTemp() {
     var urlString = url + getLocation();
-    var ctemp = app.get(api, function(req,res,next) {
+    var ctemp = app.get(urlString, function(req,res,next) {
         req.main.temp;
         res.json();
     });
