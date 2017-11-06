a6a63962dbbaee5ca3a41b215063ce05ac22a44b
ccorkery97 <ccorkery97@gmail.com>
2017-11-06 04:36:10 -0600

    weather2

--- src/components/getTemp.js
+++ src/components/getTemp.js
@@ -3,10 +3,11 @@ import getLocation from './getLocation';
 var express = require('express');
 
 var app = express();
-var api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c585309442241f836aee04f6f3118d";
+var url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c585309442241f836aee04f6f3118d";
+var api = "/api.openweathermap.org/data/2.5/forecast?id=524901&APPID=62c585309442241f836aee04f6f3118d";
 function getTemp() {
-    var urlString = api + getLocation();
-    var ctemp = app.get(urlString, function(req,res,next) {
+    var urlString = url + getLocation();
+    var ctemp = app.get(api, function(req,res,next) {
         req.main.temp;
         res.json();
     });
